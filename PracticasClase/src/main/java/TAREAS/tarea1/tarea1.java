83111ef948c6ceabd8960be78397121d3086568c
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-24 17:52:00 -0600

    Create tarea1.java

---
+++ PracticasClase/src/main/java/TAREAS/tarea1/tarea1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package TAREAS.tarea1;
+
+/**
+ *
+ * @author VIBEXZ
+ */
+public class tarea1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //-----------------------------------------EJERCICIO1 TAREA----------------------------------------------------------------
+        System.out.println(InvertirNum.invertir(12336, 0));
+
+    }
+
+}
