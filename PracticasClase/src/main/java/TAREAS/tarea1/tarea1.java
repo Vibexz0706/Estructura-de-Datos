1fd3ce4dea91fe4db642f3439253ca766be17528
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-30 14:57:30 -0600

    Update tarea1.java

--- PracticasClase/src/main/java/TAREAS/tarea1/tarea1.java
+++ PracticasClase/src/main/java/TAREAS/tarea1/tarea1.java
@@ -8,8 +8,9 @@ package TAREAS.tarea1;
  *
  * @author VIBEXZ
  */
-import static TAREAS.tarea1.EJE3.anagrama;
+//import static TAREAS.tarea1.EJE2.anagrama;
 import static TAREAS.tarea1.EJER4.recurSum;
+import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 public class tarea1 {
@@ -22,28 +23,38 @@ public class tarea1 {
         //--------------------------------------------------------------EJERCICIO 1---------------------------------------
         //System.out.println(EJER1.invertir(12336));
         //--------------------------------------------------------------EJERCICIO 2----------------------------------------
+        /*
+        String palabra = "hola";
+        System.out.println(EJE2.eje2(palabra));
+        */
         
         
-        
+        //System.out.println(anagrama(palabra,palabra.length()-1));
+         
         //--------------------------------------------------------------EJERCICIO 3----------------------------------------
         /*
-        String palabra = "Poder";
-            System.out.println(anagrama(palabra,palabra.length()-1));
+         System.out.print("Digite el tamaño ");
+         int tamaño = new java.util.Scanner(System.in).nextInt();
+         int vector []= new int [tamaño];
+         
+         System.out.println(Arrays.toString(EJE2.devolver(vector, tamaño)));
+         System.out.println(EJE2.valorMay(vector, tamaño, tamaño));
          */
         //---------------------------------------------------------------EJERCICIO 4-----------------------------------------
         /*
         
-        int n = 7;
+        int n = 10;
         System.out.println(recurSum(n));
         
-        */
+         */
+        
         //---------------------------------------------------------------EJERCICIO 5------------------------------------------
         /*
         int d;
         String val;
         val = JOptionPane.showInputDialog("Digite el numero de la tabla de multiplicar vas a utilizar");
         d = Integer.parseInt(val);
-        System.out.println(EJE5.tabla(12, d));
+        System.out.println(EJE5.tabla(10, d));
 
          */
     }
