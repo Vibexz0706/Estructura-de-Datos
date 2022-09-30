218861072afa71addd08cba78e88f59f03816b68
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-29 23:05:12 -0600

    Update prueba.java

--- PracticasClase/src/main/java/TAREAS/tarea2/prueba.java
+++ PracticasClase/src/main/java/TAREAS/tarea2/prueba.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package TAREAS.tarea1.tarea2;
+package TAREAS.tarea2;
 
 /**
  *
