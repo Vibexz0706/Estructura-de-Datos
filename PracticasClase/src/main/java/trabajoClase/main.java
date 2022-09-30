38e173e7ad24f262f6fd6885960c1264e98812ac
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-30 17:31:41 -0600

    Update main.java

--- PracticasClase/src/main/java/trabajoClase/main.java
+++ PracticasClase/src/main/java/trabajoClase/main.java
@@ -2,23 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package trabajoClase.Semana2;
+package trabajoClase;
 
 /**
  *
  * @author VIBEXZ
  */
-public class RECURSIVIDAD {
+public class main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        
+        //-----------------------------------------------------------SEMAMANA2------------------------------------------------------------------
+        /*
         //COMO EL METODO ES ESTATICO, NO SE PUEDE INSTANCIAR POR ESO SOLO SE LLAMA
         int arr[] = {12, 34, 64, 2, 3};
         
         System.out.println(Main.factorial(3));
         System.out.println(Main.encontrarMax(arr, 1, arr[0]));
+        
+        */
+        
+        
+
+
     }
 
 }
