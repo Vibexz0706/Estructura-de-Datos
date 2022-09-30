7c4dc01218bdef3da3d7a7525d614c7b9066836b
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-29 23:01:27 -0600

    Create EJER1.java

---
+++ PracticasClase/src/main/java/TAREAS/tarea1/EJER1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TAREAS.tarea1;
+
+/**
+ *
+ * @author VIBEXZ
+ */
+public class EJER1 {
+    
+    public static String invertir(int numero) {
+
+        if (numero < 10) {
+            return String.valueOf(numero);     //convertimos los valores a string para poder darles la vuelta
+                                               // si los dejamos como enteros se va hacer una suma y no va a dar el resultado
+                                               // ejemplo si lo dejamos entero y nuestras variable numero es 123 retorna 6 porque se suman por eso se pasan a string
+                                              
+            
+        } else {
+            return String.valueOf(numero % 10) + invertir(numero / 10); 
+        }
+
+    }
+
+}
