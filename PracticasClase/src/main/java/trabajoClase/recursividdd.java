f2339abde9b4df944364ee12d6a523d954ce7157
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-30 17:31:45 -0600

    Create recursividdd.java

---
+++ PracticasClase/src/main/java/trabajoClase/recursividdd.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajoClase;
+
+/**
+ *
+ * @author VIBEXZ
+ */
+public class recursividdd {
+
+    static int arr[] = {12, 34, 64, 2, 3};
+
+    public static int factorial(int n) {
+
+        if (n == 0) {
+            return 1;
+
+        } else {
+            System.out.println(n);
+            return n * factorial(n - 1);
+
+        }
+
+    }
+
+    public static int encontrarMax(int[] numeros, int indice,int max) {
+
+        if (indice != numeros.length) {
+            
+            if (numeros[indice]>max){
+                max=encontrarMax(numeros, indice+1, numeros[indice]);
+            
+            
+            }
+            else{
+                max=encontrarMax(numeros, indice+1, max);
+            
+            
+            }
+
+        }
+
+        return max;
+
+    }
+
+}
