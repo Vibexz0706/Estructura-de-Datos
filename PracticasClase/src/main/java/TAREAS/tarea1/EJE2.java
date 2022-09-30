931be5215e55d3beed3e29e3584fe1ea590f52d1
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-30 14:57:25 -0600

    Update EJE2.java

--- PracticasClase/src/main/java/TAREAS/tarea1/EJE2.java
+++ PracticasClase/src/main/java/TAREAS/tarea1/EJE2.java
@@ -5,10 +5,47 @@
 package TAREAS.tarea1;
 
 /**
- * 
- * 
+ *
+ * @author VIBEXZ
  */
 public class EJE2 {
+    /*
+     public static String anagrama(String anagrama,int tamaño)  {
+         if(tamaño==0){
+             return anagrama.charAt(tamaño)+"";
+         }else{
+             return anagrama.charAt (tamaño) + (anagrama(anagrama,tamaño -1));
+             
+       
+          }
+     }
+    
+    */
+    
+    public static char eje2 (String palabra){
+        
+        int n = 0;
+        
+        if( n < palabra.length()){
+            
+            char c = palabra.charAt(n);
+            n ++;
+            
+            return c;
+        
+        }
+        return 0;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
