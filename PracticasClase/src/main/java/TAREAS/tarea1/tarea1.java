44219fa45b9f8cbfe5b205ff29f3222a2aa66961
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-29 23:01:32 -0600

    Update tarea1.java

--- PracticasClase/src/main/java/TAREAS/tarea1/tarea1.java
+++ PracticasClase/src/main/java/TAREAS/tarea1/tarea1.java
@@ -8,6 +8,10 @@ package TAREAS.tarea1;
  *
  * @author VIBEXZ
  */
+import static TAREAS.tarea1.EJE3.anagrama;
+import static TAREAS.tarea1.EJER4.recurSum;
+import javax.swing.JOptionPane;
+
 public class tarea1 {
 
     /**
@@ -15,9 +19,33 @@ public class tarea1 {
      */
     public static void main(String[] args) {
 
-        //-----------------------------------------EJERCICIO1 TAREA----------------------------------------------------------------
-        System.out.println(InvertirNum.invertir(12336, 0));
+        //--------------------------------------------------------------EJERCICIO 1---------------------------------------
+        //System.out.println(EJER1.invertir(12336));
+        //--------------------------------------------------------------EJERCICIO 2----------------------------------------
+        
+        
+        
+        //--------------------------------------------------------------EJERCICIO 3----------------------------------------
+        /*
+        String palabra = "Poder";
+            System.out.println(anagrama(palabra,palabra.length()-1));
+         */
+        //---------------------------------------------------------------EJERCICIO 4-----------------------------------------
+        /*
+        
+        int n = 7;
+        System.out.println(recurSum(n));
+        
+        */
+        //---------------------------------------------------------------EJERCICIO 5------------------------------------------
+        /*
+        int d;
+        String val;
+        val = JOptionPane.showInputDialog("Digite el numero de la tabla de multiplicar vas a utilizar");
+        d = Integer.parseInt(val);
+        System.out.println(EJE5.tabla(12, d));
 
+         */
     }
 
 }
