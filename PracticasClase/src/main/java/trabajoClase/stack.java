0ec6628e0fc0ab75a60a1a8f1a56602109169bd7
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-30 20:48:11 -0600

    Update stack.java

--- PracticasClase/src/main/java/trabajoClase/stack.java
+++ PracticasClase/src/main/java/trabajoClase/stack.java
@@ -9,22 +9,46 @@ package trabajoClase;
  * @author VIBEXZ
  * @param <T>
  */
-public class stack<T>{
+
+
+public class stack<T> {
+    
+    
     /*
     ATRIBUTOS DE LA PILA
-    */
+     */
     private node<T> top;
-    
-    
+
     // T value es como pasar int a o string jose. solo que estamos usando java genericts.
-    
-    public void push(T value){
-        
-        node <T> newNode = new node<T> (value);
-        
-    
-    
+    public void push(T value) {
+
+        node<T> newNode = new node<T>(value);
+        if (top == null) {
+
+            top = newNode;
+
+        } else {
+
+            newNode.setNext(top);
+            top = newNode;
+
+        }
+
     }
-    
-    
+
+    //el va a retornar valor t o sea te puede ser cualquier int o strig o cualquier dato. java genericts
+    public T pop() {
+        node<T> popped_value = top;
+
+        if (top != null) {
+            top = top.getNext();
+            return popped_value.getValue();
+
+        } else {
+            return null;
+
+        }
+
+    }
+
 }
