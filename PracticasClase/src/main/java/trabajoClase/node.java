8a81b76d47878f4eb19da906e31c23346c426d06
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-09-30 20:48:06 -0600

    Update node.java

--- PracticasClase/src/main/java/trabajoClase/node.java
+++ PracticasClase/src/main/java/trabajoClase/node.java
@@ -7,7 +7,55 @@ package trabajoClase;
 /**
  *
  * @author VIBEXZ
+ * @param <T>
  */
-public class node {
+
+
+
+        // java genericts <T>, definimos los atributos sin el tipo de dato. cuando instanciamos el node le definos el tipo de dato
+        // ejemplo private node<T> color;  color puede ser int, string lo que sea gracias al java genericts.
+
+public class node<T> {
+    
+    private T value;
+    private node<T> next;
+    private node<T> back;
+
+    public node() {
+      
+    }
+    public node(T value) {
+        
+        this.value = value;
+      
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(node<T> next) {
+        this.next = next;
+    }
+
+    public node<T> getBack() {
+        return back;
+    }
+
+    public void setBack(node<T> back) {
+        this.back = back;
+    }
+    
+    
+    
+    
     
 }
