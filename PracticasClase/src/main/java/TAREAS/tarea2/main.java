a9eb24970a206fc32391ee70ee89fb9f1190d661
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-10-12 19:38:12 -0600

    Update main.java

---
+++ PracticasClase/src/main/java/TAREAS/tarea2/main.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package TAREAS.tarea2;
+
+/**
+ *
+ * @author VIBEXZ
+ */
+import java.util.Scanner;
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //-------------------------------------------------------EJERCICIO 1 SERIE DE TALOR-----------------------------------------------------------
+        /*
+          System.out.print(SerieTaylor.coseno(15, 4) + "\n" );
+       
+         */
+        //----------------------------------------------------------------EJERCICIO DE PILAS--------------------------------------------------
+        Scanner Tecla = new Scanner(System.in);
+        pila pila1 = new pila();
+
+        System.out.print(" \n Cuantos datos desas ingresas: \n");
+        int a = Tecla.nextInt();
+        
+
+        for (int i = 0;  i < a ; i++) {
+
+            pila1.ingresarNodo();
+            
+        }
+
+        
+
+       System.out.println(" \n LA LISTA DE CAMISAS ES LA SIGUIENTE: \n");
+       pila1.desplegarPila();
+       
+       System.out.println(" \n CANTIDAD DE TALLAS REPETIDAS : \n");
+       pila1.tallasRep();
+       
+    }
+
+}
